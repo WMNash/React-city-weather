82812be55d078f3ce0840781c704fd11761923e9
W.M.Nasrullah <102357246+WMNash@users.noreply.github.com>
2022-09-13 10:24:07 +0800

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': '7e6be5ae72msh16752d6a7cf2366p187162jsn7ce314386e55',
+        'X-RapidAPI-Key': '(your key here)',
         'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
     }
 };
@@ -10,4 +10,4 @@ export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
 
 export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
 
-export const WEATHER_API_KEY = 'a791a590ecfd8320c6064f609e18b6d7';
+export const WEATHER_API_KEY = '(your key here)';
